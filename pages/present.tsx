fb26495ab3dffcc9b49d7a7bd9714b5b805ddcb7
lonkaars <loek@pipeframe.xyz>
2021-05-12 12:43:19 +0200

    present screen working-ish

---
+++ pages/present.tsx
@@ -0,0 +1,7 @@
+export default function Present() {
+	return <div className="presentation posfix a0 h100vh">
+		<div className="slideWrapper abscenterv posrel">
+			<div className="slide"></div>
+		</div>
+	</div>;
+}
