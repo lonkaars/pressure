212d37ed1947a7d9dddd66d206cafcdc063ee37f
lonkaars <loek@pipeframe.xyz>
2021-05-14 12:30:22 +0200

    added buttons :tada:

--- pages/editor.tsx
+++ pages/editor.tsx
@@ -1,7 +1,14 @@
 import AppBar from '@material-ui/core/AppBar';
+import Fab from '@material-ui/core/Fab';
 import Toolbar from '@material-ui/core/Toolbar';
 
 import { PressureIcon } from '../components/icons';
+
+import FullscreenRoundedIcon from '@material-ui/icons/FullscreenRounded';
+import NavigateBeforeRoundedIcon from '@material-ui/icons/NavigateBeforeRounded';
+import NavigateNextRoundedIcon from '@material-ui/icons/NavigateNextRounded';
+import PauseRoundedIcon from '@material-ui/icons/PauseRounded';
+import SkipPreviousRoundedIcon from '@material-ui/icons/SkipPreviousRounded';
 
 export default function Index() {
 	return <>
@@ -13,7 +20,20 @@ export default function Index() {
 				</Toolbar>
 			</AppBar>
 			<div className='settings'></div>
-			<div className='viewer'></div>
+			<div className='viewer'>
+				<div className='player'></div>
+				<div className='controls'>
+					<div className='posabs abscenter'>
+						<Fab size='small' children={<SkipPreviousRoundedIcon />} />
+						<Fab className='playPause' size='medium' children={<PauseRoundedIcon />} />
+						<Fab size='small' children={<NavigateBeforeRoundedIcon />} />
+						<Fab size='small' children={<NavigateNextRoundedIcon />} />
+					</div>
+					<div className='posabs abscenterv r0'>
+						<Fab size='small' children={<FullscreenRoundedIcon />} />
+					</div>
+				</div>
+			</div>
 			<div className='tools'></div>
 			<div className='timeline'></div>
 		</div>
